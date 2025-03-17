e024255bfcfc11186989d1c75b90ef4a05330729
Ulhaq17 <ulhaqmdhiya17@gmail.com>
2025-03-17 16:20:57 +0700

    almost finishing login page

--- gaskuy/src/pages/Auth/Login.jsx
+++ gaskuy/src/pages/Auth/Login.jsx
@@ -11,7 +11,24 @@ const Login = () => {
   const navigate = useNavigate();
 
   // Handle login form submit
-  const handleLogin = async (e) => {}
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+    if (!validateEmail(email)) {
+      setError("Silahkan masukkan alamat email");
+      return;
+    }
+
+    if (!password) {
+      setError("Silahkan masukkan password");
+      return;
+    }
+
+    setError("");
+
+    // Login API Call
+  }
+  
   return (
     <div>
       <div className='h-3/4 mt-[100px] md:h-full flex flex-col justify-center text-center'>
@@ -20,55 +37,55 @@ const Login = () => {
           <span className='bg-[linear-gradient(to_bottom,transparent_50%,#AAEEC4_50%)] px-1'>Selamat datang</span> di Gasskuy, login untuk melanjutkan!
         </p>
 
-        <div className='lg:w-[30%] h-3/4 md:h-full flex flex-col justify-center text-left'>
-          <form onSubmit={handleLogin}>
-            <Input
-              value={email}
-              onChange={({ target }) => setEmail(target.value)}
-              label="Email"
-              placeholder="gasskuy@gmail.com"
-              text="text"
-            />
+        <div className='flex justify-center items-center'>
+          <div className='lg:w-[30%] md:w-[50%] w-[80%] flex flex-col justify-center text-left'>
+            <form onSubmit={handleLogin}>
+              <Input
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                label="Email"
+                placeholder="gasskuy@gmail.com"
+                type="text"
+              />
 
-            <Input
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-              label="Password"
-              placeholder="Minimal 8 Karakter"
-              text="password"
-            />
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Minimal 8 Karakter"
+                type="password"
+              />
 
-            {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+              {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
 
-            <div className='flex justify-between items-center text-[16px] text-black mt-3 mb-10'>
-              <label className='flex items-center'>
-                <input
-                  type="checkbox"
-                  checked={rememberMe}
-                  onChange={() => setRememberMe(!rememberMe)}
-                  className="mr-2"
-                />
-                Ingat saya
-              </label>
-              <Link className='font-normal text-[#3932FF]' to='/forgot-password'>
-                Lupa password?
-              </Link>
-            </div>
-            
-            <button type='submit' className='btn-auth'>
-              LOGIN
-            </button>
+              <div className='flex justify-between items-center text-[16px] text-black mt-3 mb-10'>
+                <label className='flex items-center'>
+                  <input
+                    type="checkbox"
+                    checked={rememberMe}
+                    onChange={() => setRememberMe(!rememberMe)}
+                    className="mr-2"
+                  />
+                  Ingat saya
+                </label>
+                <Link className='font-normal text-[#3932FF]' to='/forgot-password'>
+                  Lupa password?
+                </Link>
+              </div>
+              
+              <button type='submit' className='btn-auth w-full'>
+                LOGIN
+              </button>
 
-            <p className='text-[16px] text-center text-black mt-3'>
-              Belum punya akun? {" "}
-            <Link className='font-medium text-[#3932FF]' to='/register'>
-            daftar disini
-            </Link>
-            </p>
-          </form>
+              <p className='text-[16px] text-center text-black mt-3'>
+                Belum punya akun? {" "}
+                <Link className='font-medium text-[#3932FF]' to='/register'>
+                  daftar disini
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
-
-
       </div>
     </div>
   );
