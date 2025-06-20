227010b0e5d40c8451b9904b1ee4a39bf37d6dfc
Ulhaq17 <ulhaqmdhiya17@gmail.com>
2025-06-20 13:19:42 +0700

    fix register api

--- gaskuy/src/pages/Auth/Register.jsx
+++ gaskuy/src/pages/Auth/Register.jsx
@@ -65,7 +65,7 @@ const Register = () => {
 
     // Register API Call
     try {
-      const res = await API.post("/auth/register", {
+      const res = await API.post("/auth/signup", {
         fullName: fullName,
         email: email,
         password: password,
